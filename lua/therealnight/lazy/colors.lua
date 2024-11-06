722169115155f943314642f81fa23450a3cfc5e0
Blaz Ocepek <blaz.ocepek@adacta-fintech.com>
2024-11-06 11:21:45 +0100

    Fix colors.

--- lua/therealnight/lazy/colors.lua
+++ lua/therealnight/lazy/colors.lua
@@ -13,6 +13,7 @@ return {
         config = function()
             ColorMyPencils()
             require('kanagawa').setup({
+                force_reverse_video_cursor = true,
                 compile = false,             -- enable compiling the colorscheme
                 undercurl = true,            -- enable undercurls
                 commentStyle = { italic = true },
@@ -24,11 +25,56 @@ return {
                 dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
                 terminalColors = true,       -- define vim.g.terminal_color_{0,17}
                 colors = {                   -- add/modify theme and palette colors
-                    palette = {},
+                    palette = {
+                        sumiInk0 = "#000000",
+                        fujiWhite = "#FFFFFF",
+                    },
                     theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+                    foreground = "#dcd7ba",
+                    background = "#1f1f28",
+
+                    cursor_bg = "#c8c093",
+                    cursor_fg = "#c8c093",
+                    cursor_border = "#c8c093",
+
+                    selection_fg = "#c8c093",
+                    selection_bg = "#2d4f67",
+
+                    scrollbar_thumb = "#16161d",
+                    split = "#16161d",
+
+                    ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+                    brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+                    indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
                 },
                 overrides = function(colors) -- add/modify highlights
-                    return {}
+                    local theme = colors.theme
+                    return {
+                        NormalFloat = { bg = "none" },
+                        FloatBorder = { bg = "none" },
+                        FloatTitle = { bg = "none" },
+
+                        -- Save an hlgroup with dark background and dimmed foreground
+                        -- so that you can use it where your still want darker windows.
+                        -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+                        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+                        -- Popular plugins that open floats will link to NormalFloat by default;
+                        -- set their background accordingly if you wish to keep them dark and borderless
+                        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                        TelescopeTitle = { fg = theme.ui.special, bold = true },
+                        TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                        TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+                        TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+                        TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+                        TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+                        TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+                        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },  -- add `blend = vim.o.pumblend` to enable transparency
+                        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                        PmenuSbar = { bg = theme.ui.bg_m1 },
+                        PmenuThumb = { bg = theme.ui.bg_p2 },
+                    }
                 end,
                 theme = "dragon",              -- Load "wave" theme when 'background' option is not set
                 background = {               -- map the value of 'background' option to a theme
