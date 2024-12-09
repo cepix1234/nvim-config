7f83d75fb776e29378906ea2ff252a3793956cc2
cepix1234 <bocepek027@gmail.com>
2024-12-09 09:25:49 +0100

    Add layzi git remove diff view.

--- lua/therealnight/lazy/telescope.lua
+++ lua/therealnight/lazy/telescope.lua
@@ -28,5 +28,6 @@ return {
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        require("telescope").load_extension("lazygit")
     end
 }
