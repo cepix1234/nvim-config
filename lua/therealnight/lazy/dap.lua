ca374fbd0be10d10f9ca9e8cf4ab9ebc3371f1a7
cepix1234 <bocepek027@gmail.com>
2025-01-07 22:28:09 +0100

    Add debuging for node

---
+++ lua/therealnight/lazy/dap.lua
@@ -0,0 +1,98 @@
+return {
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            {
+                "williamboman/mason.nvim",
+                opts = function(_, opts)
+                    opts.ensure_installed = opts.ensure_installed or {}
+                    table.insert(opts.ensure_installed, "js-debug-adapter")
+                end,
+            },
+        },
+        config = function()
+            local dap = require("dap")
+            if not dap.adapters["pwa-node"] then
+                require("dap").adapters["pwa-node"] = {
+                    type = "server",
+                    host = "localhost",
+                    port = "${port}",
+                    executable = {
+                        command = "node",
+                        -- 💀 Make sure to update this path to point to your installation
+                        args = {
+                            require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+                                .. "/js-debug/src/dapDebugServer.js",
+                            "${port}",
+                        },
+                    },
+                }
+            end
+            if not dap.adapters["node"] then
+                dap.adapters["node"] = function(cb, config)
+                    if config.type == "node" then
+                        config.type = "pwa-node"
+                    end
+                    local nativeAdapter = dap.adapters["pwa-node"]
+                    if type(nativeAdapter) == "function" then
+                        nativeAdapter(cb, config)
+                    else
+                        cb(nativeAdapter)
+                    end
+                end
+            end
+
+            for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
+                if not dap.configurations[language] then
+                    dap.configurations[language] = {
+                        {
+                            type = "pwa-node",
+                            request = "launch",
+                            name = "Launch file",
+                            program = "${file}",
+                            cwd = "${workspaceFolder}",
+                        },
+                        {
+                            type = "pwa-node",
+                            request = "attach",
+                            name = "Attach",
+                            processId = require("dap.utils").pick_process,
+                            cwd = "${workspaceFolder}",
+                        },
+                        {
+                            type = 'pwa-node',
+                            request = 'attach',
+                            name = 'Atach 9229',
+                            port = 9229,
+                        }
+                    }
+                end
+            end
+            vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+            vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+            vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+            vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+            vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+            vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
+            vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+            vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+            vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+            vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+                require('dap.ui.widgets').hover()
+            end)
+            vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+                require('dap.ui.widgets').preview()
+            end)
+            vim.keymap.set('n', '<Leader>df', function()
+                local widgets = require('dap.ui.widgets')
+                widgets.centered_float(widgets.frames)
+            end)
+            vim.keymap.set('n', '<Leader>ds', function()
+                local widgets = require('dap.ui.widgets')
+                widgets.centered_float(widgets.scopes)
+            end)
+
+
+        end,
+    }
+}
