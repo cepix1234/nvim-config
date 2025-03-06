ccd49f1dc119550ab7f090a6e032304aaf990203
cepix1234 <bocepek027@gmail.com>
2025-03-06 21:29:52 +0100

    Add git worktree navigation

---
+++ lua/the-real-night/lazy/git-integration/git-worktrees.lua
@@ -0,0 +1,32 @@
+return {
+    'polarmutex/git-worktree.nvim',
+    version = '^2',
+    dependencies = { 'nvim-lua/plenary.nvim', 'stevearc/overseer.nvim' },
+    config = function()
+        vim.g.git_worktree_log_level = "trace"
+        require('telescope').load_extension('git_worktree')
+
+        local Hooks = require 'git-worktree.hooks'
+        local config = require('git-worktree.config')
+        local update_on_switch = Hooks.builtins.update_current_buffer_on_switch
+
+        Hooks.register(Hooks.type.DELETE, function()
+            vim.cmd(config.update_on_change_command)
+        end)
+
+        Hooks.register(Hooks.type.SWITCH, function(path, prev_path)
+            vim.notify('Moved:' .. prev_path .. '  ~>  ' .. path)
+            update_on_switch(path, prev_path)
+        end)
+
+        local switch_worktree = function()
+            require('telescope').extensions.git_worktree.git_worktree()
+        end
+        local create_worktree = function()
+            require('telescope').extensions.git_worktree.create_git_worktree()
+        end
+
+        vim.keymap.set('n', '<leader>wG', create_worktree)
+        vim.keymap.set('n', '<leader>wg', switch_worktree)
+    end
+}
